c802f454a0ee7932569935c7bdcc361637ebe111
BeaSN <130484933+BeaSN@users.noreply.github.com>
2023-08-11 15:56:22 -0300

    Ajustes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 class TeamsController < ApplicationController
-  layout 'container'
+  layout "container"
   before_action :connection_api
 
   def index
-    response = @connection.get('get_teams_by_course_id', { course_id: params[:course_id].to_i })
+    response = @connection.get("get_teams_by_course_id", { course_id: params[:course_id].to_i })
     @teams = JSON.parse(response.body)
     render "body"
   end
 
   private
+
   def connection_api
-    base_url = 'http://localhost:3001/api/school/v1/teams'
+    base_url = "#{root_url}/api/school/v1/teams"
     @connection = Faraday.new(url: base_url)
   end
 end
